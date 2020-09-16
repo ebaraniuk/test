e118663a16aed5086ee8524eb7b12ba6d03ec963
katyushka02 <ebaraniuk02@gmail.com>
2020-09-16 17:58:31 +0300

    commit2

---
+++ TestApp.py
@@ -0,0 +1,14 @@
+import random
+import numpy as np
+
+
+def array():
+    a = np.array([[random.randint(0, 10) for i in range(4)], [random.randint(0, 10) for i in range(4)],
+                  [random.randint(0, 10) for i in range(4)], [random.randint(0, 10) for i in range(4)]])
+    return a
+
+
+def mult():
+    a = array()
+    b = array()
+    print(a, b, np.dot(a, b))
